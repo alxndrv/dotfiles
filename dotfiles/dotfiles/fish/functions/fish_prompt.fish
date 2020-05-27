24d787d8a972ffe3c531422e51b4866681744750
Andrey Aleksandrov <andrey.aleksandrov@visma.com>
2020-05-27 11:31:46 +0300

    Remove files placed in incorrect locations

--- dotfiles/dotfiles/fish/functions/fish_prompt.fish
+++
@@ -1,3 +0,0 @@
-function fish_prompt
-    printf '%s%s%s:%s%s/%s$ ' (set_color green) "$USER" (set_color normal) (set_color blue) (prompt_pwd) (set_color normal)
-end
--- dotfiles/dotfiles/fish/functions/la.fish
+++
@@ -1,3 +0,0 @@
-function la
-    ls -lha $argv
-end
