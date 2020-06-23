9b66015e7f28ddf12e46c811e96e2776488c92c6
Andrey Aleksandrov <andrey.aleksandrov0@gmail.com>
2020-06-23 15:36:22 +0300

    Update polybar config for multiple monitors, fix typo in sxhkdrc

--- dotfiles/polybar/launch.sh
+++ dotfiles/polybar/launch.sh
@@ -7,4 +7,6 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar alx
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload alx &
+done
