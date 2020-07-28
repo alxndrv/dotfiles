ab4a77db2984e38910b40a3350442de448c3fafa
Andrey Aleksandrov <andrey.aleksandrov@visma.com>
2020-07-28 13:14:40 +0300

    Load local.fish on fish startup

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -36,3 +36,6 @@ set -U SXHKD_SHELL /usr/bin/bash
 set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
 
 alias ytdla="youtube-dl --add-metadata -i -x -f bestaudio/best"
+
+
+source ~/.config/fish/local.fish
