89ad5064194c4b5a6223753d9f8bdb917a2d148b
alxndrv <>
2024-02-28 21:28:56 +0200

    Add basic Neovim config

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,15 @@
+-- Tab stuff
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true 
+
+-- UI config
+vim.opt.number = true
+vim.opt.relativenumber = false
+vim.opt.cursorline = false
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- Searching
+vim.opt.hlsearch = true
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,36 @@
+return {
+	{
+		"hrsh7th/nvim-cmp",
+		version = false, -- last release is way too old
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		version = false, -- last release is way too old and doesn't work on Windows
+		build = ":TSUpdate",
+		init = function(plugin)
+			require("lazy.core.loader").add_to_rtp(plugin)
+			require("nvim-treesitter.query_predicates")
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup()
+		end,
+	},
+	{ "sbdchd/neoformat", cmd = { "Neoformat" } },
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		config = function()
+			require("nvim-surround").setup()
+		end,
+	},
+}
