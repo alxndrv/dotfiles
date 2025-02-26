58a4041c2f2ceb2158ee8020a1223c7d6d536dfa
Andrey Aleksandrov <andrey.aleksandrov@visma.com>
2025-02-26 15:34:43 +0200

    Add script for getting GKE credentials

---
+++ scripts/get_gke_credentials.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+PROJECT=$(gcloud projects list --format "value(name)" | gum choose --header="Choose a project:")
+
+CLUSTER=$(gcloud container clusters list --project $PROJECT --format "value(name, location)" | gum choose --header "Choose a cluster:")
+
+CLUSTER_NAME=$(echo $CLUSTER | awk '{print $1}')
+CLUSTER_REGION=$(echo $CLUSTER | awk '{print $2}')
+
+COMMAND="gcloud container clusters get-credentials $CLUSTER_NAME --region $CLUSTER_REGION --project $PROJECT"
+
+gum confirm "$COMMAND" && eval "$COMMAND"
+
+NEW_NAME=$(gum input --header "Rename cluster entry $(kubectx -c):" --placeholder "$(kubectx -c)")
+
+kubectx "$NEW_NAME"=.
