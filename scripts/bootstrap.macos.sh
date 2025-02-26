1178ef814c48d16489d53cd8d7acbef3f0a47794
Andrey Aleksandrov <andrey.aleksandrov@visma.com>
2025-02-26 15:33:50 +0200

    Add alternative to yabai space switching

--- scripts/bootstrap.macos.sh
+++ scripts/bootstrap.macos.sh
@@ -30,6 +30,15 @@ defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 
 defaults write com.apple.WindowManager EnableStandardClickToShowDesktop -bool false
 
+# Get rid of as much animation delay as possible
+defaults write com.apple.finder DisableAllAnimations -bool true
+defaults write com.apple.dock launchanim -bool false
+defaults write -g NSWindowResizeTime -float 0.001
+defaults write com.apple.dock expose-animation-duration -float 0.1
+defaults write com.apple.dock autohide-time-modifier -float 0
+defaults write com.apple.universalaccess reduceMotion -bool true
+defaults write com.apple.Accessibility ReduceMotionEnabled -bool true
+
 # Disable all hot-corners
 defaults write com.apple.dock wvous-bl-corner -int 0
 defaults write com.apple.dock wvous-br-corner -int 0
